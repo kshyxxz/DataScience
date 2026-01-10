e3362706084116dcb7b55e8e00265e532ebace80
kshyxxz <kshyxxz@gmail.com>
2026-01-10 18:34:34 +0545

    lambda:MapFilterSorted

--- pyPractice/lambda.py
+++ pyPractice/lambda.py
@@ -1,12 +1,7 @@
 numbers = [23,2,14,54,35,21,85,11,24,26]
-# print(list(map(lambda x: x**2, numbers)))
-# print(list(filter(lambda x: x%3 == 0, numbers)))
-# print(list(sorted(numbers, key=lambda x: x, reverse=True)))
-try:
-    x = int("abc")
-except ValueError:
-    print("Error")
-finally:
-    print("Done")
-import logging
-logging.info("Message")
+
+print(list(map(lambda x: x**2, numbers)))
+
+print(list(filter(lambda x: x%3 == 0, numbers)))
+
+print(list(sorted(numbers, key=lambda x: x, reverse=True)))
