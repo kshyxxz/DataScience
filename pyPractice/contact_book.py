803b9f9f1c5cd01254188aded161937442381ffa
kshyxxz <kshyxxz@gmail.com>
2026-02-13 19:32:55 +0545

    python contact_book2

--- pyPractice/contact_book.py
+++ pyPractice/contact_book.py
@@ -21,15 +21,54 @@ def view_contacts():
 		print("\n--- Contact List ---")
 		for name,details in contacts.items():
 			print(f'Name : {name}')
-			print(f'Phone : {details['phone']}')
-			print(f'Email : {details['email']}')
+			print(f'Phone : {details["phone"]}')
+			print(f'Email : {details["email"]}')
 	else:
 		print('Your contact book is empty!')
 
 def search_contact():
-	name = input('Enter the contact name!')
+	name = input('Enter the contact name : ')
 	if name in contacts:
 		print(f'\n--- Contact Details for {name}---')
 		print(f'Name : {name}')
-		print(f'Phone : {contacts[name]['phone']}')
-		print(f'Name : {contacts[name]['email']}')
+		print(f'Phone : {contacts[name]["phone"]}')
+		print(f'Email : {contacts[name]["email"]}')
+	else:
+		print(f'{name} not found in your contacts!')
+
+def edit_contact():
+	name = input('Enter the contact name to edit : ')
+	if name in contacts:
+		phone = input('Enter new phone number : ')
+		email = input('Enter new email address : ')
+		contacts[name] = {"phone":phone, "email":email}
+		print(f'{name} has been updated!')
+	else:
+		print(f'{name} not found in your contacts!')
+
+def delete_contact():
+	name = input('Enter the contact name to delete : ')
+	if name in contacts:
+		del contacts[name]
+		print(f'{name} has been deleted from your contacts!')
+	else:
+		print(f'{name} not found in your contacts!')
+
+while True:
+	show_menu()
+	choice = input('Enter your choice (1-6) : ')
+	if choice == '1':
+		add_contact()
+	elif choice == '2':
+		view_contacts()
+	elif choice == '3':
+		search_contact()
+	elif choice == '4':
+		edit_contact()
+	elif choice == '5':
+		delete_contact()
+	elif choice == '6':
+		print('Exiting Contact Book. Goodbye!')
+		break
+	else:
+		print('Invalid choice! Please enter a number between 1 and 6.')
